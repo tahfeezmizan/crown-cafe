8244addd2d61acbdc2285fef629b273a58086dc6
tahfeezmizan <mizanahmed5316@gmail.com>
2024-05-20 03:28:20 +0600

    get menu all data form database

--- crown-cafe-server/index.js
+++ crown-cafe-server/index.js
@@ -26,9 +26,22 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const menuCollection = client.db("Crown-cafeDB").collection("menu")
+        const reviewsCollection = client.db("Crown-cafeDB").collection("reviews")
+
+        // get api
+        app.get('/menu', async (req, res) => {
+            try {
+                const result = await menuCollection.find().toArray();
+                res.send(result);
+            } catch (error) {
+                console.error("Error fetching queries:", error);
+                res.status(500).send("Error fetching queries");
+            }
+        })
 
 
-
+       
 
 
         // Send a ping to confirm a successful connection
